846bd8cee1ad631d2a40d315d9eac5c6d03c6f91
Daniel Arredondo <edanielarredondo@gmail.com>
2020-06-26 20:07:08 -0500

    added google chart graph

--- resources/js/index.js
+++ resources/js/index.js
@@ -97,140 +97,7 @@ am4core.ready(function () {
    // -------- LINE CHART --------- //
 
 
-   var lineChart = am4core.create("linechartdiv", am4charts.XYChart);
 
-   //insert data
-   lineChart.data = [{
-      "ax": 1,
-      "ay": 0.5,
-      "bx": 1,
-      "by": 2.2
-   }, {
-      "ax": 2,
-      "ay": 1.3,
-      "bx": 2,
-      "by": 4.9
-   }, {
-      "ax": 3,
-      "ay": 2.3,
-      "bx": 3,
-      "by": 5.1
-   }, {
-      "ax": 4,
-      "ay": 2.8,
-      "bx": 4,
-      "by": 5.3
-   }, {
-      "ax": 5,
-      "ay": 3.5,
-      "bx": 5,
-      "by": 6.1
-   }, {
-      "ax": 6,
-      "ay": 5.1,
-      "bx": 6,
-      "by": 8.3
-   }, {
-      "ax": 7,
-      "ay": 6.7,
-      "bx": 7,
-      "by": 10.5
-   }, {
-      "ax": 8,
-      "ay": 8,
-      "bx": 8,
-      "by": 12.3
-   }, {
-      "ax": 9,
-      "ay": 8.9,
-      "bx": 9,
-      "by": 14.5
-   }, {
-      "ax": 10,
-      "ay": 9.7,
-      "bx": 10,
-      "by": 15
-   }, {
-      "ax": 11,
-      "ay": 10.4,
-      "bx": 11,
-      "by": 18.8
-   }, {
-      "ax": 12,
-      "ay": 11.7,
-      "bx": 12,
-      "by": 19
-   }];
-
-   // x-axis properties
-   var valueAxisX = lineChart.xAxes.push(new am4charts.ValueAxis());
-   valueAxisX.title.text = 'Year';
-   valueAxisX.renderer.minGridDistance = 40;
-
-   // y-axis properties
-   var valueAxisY = lineChart.yAxes.push(new am4charts.ValueAxis());
-   valueAxisY.title.text = 'Total Messages';
-
-   // Create series
-   var lineSeries = lineChart.series.push(new am4charts.LineSeries());
-   lineSeries.dataFields.valueY = "ay";
-   lineSeries.dataFields.valueX = "ax";
-   lineSeries.strokeOpacity = 0;
-
-   var lineSeries2 = lineChart.series.push(new am4charts.LineSeries());
-   lineSeries2.dataFields.valueY = "by";
-   lineSeries2.dataFields.valueX = "bx";
-   lineSeries2.strokeOpacity = 0;
-
-   // Add a bullet
-   var bullet = lineSeries.bullets.push(new am4charts.Bullet());
-
-   // Add a triangle to act as an arrow
-   var arrow = bullet.createChild(am4core.Triangle);
-   arrow.horizontalCenter = "middle";
-   arrow.verticalCenter = "middle";
-   arrow.strokeWidth = 0;
-   arrow.fill = chart.colors.getIndex(0);
-   arrow.direction = "top";
-   arrow.width = 12;
-   arrow.height = 12;
-
-   // Add a bullet
-   var bullet2 = lineSeries2.bullets.push(new am4charts.Bullet());
-
-   // Add a triangle to act as an arrow
-   var arrow2 = bullet2.createChild(am4core.Triangle);
-   arrow2.horizontalCenter = "middle";
-   arrow2.verticalCenter = "middle";
-   arrow2.rotation = 180;
-   arrow2.strokeWidth = 0;
-   arrow2.fill = chart.colors.getIndex(3);
-   arrow2.direction = "top";
-   arrow2.width = 12;
-   arrow2.height = 12;
-
-   //add the trendlines
-   var trend = lineChart.series.push(new am4charts.LineSeries());
-   trend.dataFields.valueY = "value2";
-   trend.dataFields.valueX = "value";
-   trend.strokeWidth = 2
-   trend.stroke = chart.colors.getIndex(0);
-   trend.strokeOpacity = 0.7;
-   trend.data = [
-      { "value": 1, "value2": 2 },
-      { "value": 12, "value2": 11 }
-   ];
-
-   var trend2 = lineChart.series.push(new am4charts.LineSeries());
-   trend2.dataFields.valueY = "value2";
-   trend2.dataFields.valueX = "value";
-   trend2.strokeWidth = 2
-   trend2.stroke = chart.colors.getIndex(3);
-   trend2.strokeOpacity = 0.7;
-   trend2.data = [
-      { "value": 1, "value2": 1 },
-      { "value": 12, "value2": 19 }
-   ];
 
    /* ---------- CIRCLE CHART -------------- */
 
@@ -287,3 +154,36 @@ am4core.ready(function () {
    pieSeries.hiddenState.properties.startAngle = -90;
 
 }); // end am4core.ready()
+
+google.charts.load('current', { 'packages': ['corechart'] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+   // currentYearMessageNumber = followingYearJanNummber - currentYearJanNumber
+   var data = google.visualization.arrayToDataTable([
+      ['Year', 'Aaron', 'James', 'Dan', 'Thomas', 'Bryan', 'Chandler', 'Hector', 'Ryan'],
+      ['2015', 49, 0, 46, 27, 28, 0, 0, 0],
+      ['2016', 9762, 8041, 4423, 4416, 1955, 1639, 0, 857],
+      ['2017', 13386, 9495, 3872, 4340, 4220, 1377, 712, 750],
+      ['2018', 14384, 16667, 9651, 2251, 3487, 1109, 4371, 754],
+      ['2019', 8178, 9273, 1279, 1976, 324, 5604, 4349, 315 ],
+      ['2020', 7045, 4567, 1144, 1888, 3218, 5327, 2215, 71]
+   ]);
+
+   var options = {
+      title: '',
+      curveType: 'function',
+      legend: { position: 'bottom' }
+   };
+
+   var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+   chart.draw(data, {viewWindow:{min:0, max: 20000}}, options);
+}
+
+$(window).resize(function(){
+   drawChart1();
+   drawChart2();
+ });
+ 
